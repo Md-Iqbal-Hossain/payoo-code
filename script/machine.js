b3dd73d6b03e1b500261e67bb6f879dd2f570f0c
Iqbal <iqbalhossain6615@gmail.com>
2026-02-25 02:53:38 +0600

    hide filtered button and show according to the button is done for add money and cashout

--- script/machine.js
+++ script/machine.js
@@ -16,8 +16,22 @@ function getBalance(){
 }
 
 //machine value -> set Balance
-
 function setBalance(value){
     const balanceElement = document.getElementById('balance');
     balanceElement.innerText = value;
+}
+
+// machine id -> hide all > show id 
+function showOnly(id){
+    const addmoney = document.getElementById('add-money');
+    const cashout = document.getElementById('cashout');
+    // console.log('show only clicked');
+    // sobai k hide kore dao
+    addmoney.classList.add('hidden');
+    cashout.classList.add('hidden');
+
+    // id wala element ta ke tumi show korao
+    const selected = document.getElementById(id);
+    selected.classList.remove('hidden');
+
 }
